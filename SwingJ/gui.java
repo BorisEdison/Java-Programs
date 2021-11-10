6ac03d663f8cf7befd1cfbbdda30f0087f48dc4a
Boris Edison <67020745+BorisEdison@users.noreply.github.com>
2021-11-10 19:47:57 +0530

    Add files via upload

---
+++ SwingJ/gui.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class gui {
+    public static void main(String[] args){
+        JFrame f  = new JFrame("Swing Demo");
+
+        JButton b = new JButton("Click");
+
+        b.setBounds(130,100,100,40);
+
+        f.add(b);
+
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+    
+}
